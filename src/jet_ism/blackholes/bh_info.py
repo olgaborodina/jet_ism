f1098739b3823b8d37c8a023184dc9a5f7d7173f
olgaborodina <ol.borodina@icloud.com>
2025-10-01 17:06:37 -0400

    Remove delimiter warning

--- src/jet_ism/blackholes/bh_info.py
+++ src/jet_ism/blackholes/bh_info.py
@@ -11,7 +11,7 @@ def blackholes(output_directory):
     Input: output_directory (directory where snapshots are stored)
     Output: black_hole_info (pandas dataframe with the black hole information, time in Myr, and jetpower column in erg/s )
     """
-    black_hole_info = pd.read_csv(output_directory+'blackholes.txt', delimiter='\s+', names=['time', 'num', 'mass', 'mdot', 'mdot_msun_y', 'mass_tot_real', 'mdot_edd'])
+    black_hole_info = pd.read_csv(output_directory+'blackholes.txt', delimiter=r'\s+', names=['time', 'num', 'mass', 'mdot', 'mdot_msun_y', 'mass_tot_real', 'mdot_edd'])
     black_hole_info['time'] *= unit_time_in_megayr
     black_hole_info['jetpower'] = jetpower_from_accretion(black_hole_info['mdot_msun_y'] / cgs2solarperyear_accretion(1))    
     return black_hole_info
@@ -23,5 +23,5 @@ def blackhole_details(output_directory, number):
     Output: black_hole_details (pandas dataframe with the black hole details)
     """
     detailes_directory = output_directory + "/blackhole_details/"
-    black_hole_details = pd.read_csv(detailes_directory+f'blackhole_details_{number}.txt', delimiter='\s+', names=['id', 'time', 'mass', 'mdot', 'mdot_msun_y', 'mass_tot_real', 'epsilon'])
+    black_hole_details = pd.read_csv(detailes_directory+f'blackhole_details_{number}.txt', delimiter=r'\s+', names=['id', 'time', 'mass', 'mdot', 'mdot_msun_y', 'mass_tot_real', 'epsilon'])
     return black_hole_details
