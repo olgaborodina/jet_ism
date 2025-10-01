f9248ea0b1a7b23c1a59e7d4f140cad919ffabc0
olgaborodina <ol.borodina@icloud.com>
2025-10-01 13:38:36 -0400

    Add jet power to the bh info

--- src/jet_ism/blackholes/bh_info.py
+++ src/jet_ism/blackholes/bh_info.py
@@ -2,15 +2,18 @@ import numpy as np    # scientific computing package
 import pandas as pd
 import scipy as scp
 
+from .accretion import *
 from .. import (unit_time_in_megayr)
 
 def blackholes(output_directory):
     """
     Read the black hole information from the blackholes.txt file in the output directory
     Input: output_directory (directory where snapshots are stored)
-    Output: black_hole_info (pandas dataframe with the black hole information)
+    Output: black_hole_info (pandas dataframe with the black hole information, time in Myr, and jetpower column in erg/s )
     """
     black_hole_info = pd.read_csv(output_directory+'blackholes.txt', delimiter='\s+', names=['time', 'num', 'mass', 'mdot', 'mdot_msun_y', 'mass_tot_real', 'mdot_edd'])
+    black_hole_info['time'] *= unit_time_in_megayr
+    black_hole_info['jetpower'] = jetpower_from_accretion(black_hole_info['mdot_msun_y'] / cgs2solarperyear_accretion(1))    
     return black_hole_info
 
 def blackhole_details(output_directory, number):
